8282b94c67379d4bdb6a9593bc960a8db6c8fe18
Hannah VanderHoeven <hannahvanderh@gmail.com>
2022-10-14 20:01:58 -0600

    working on usopp, basic csv generation

--- handTracking.py
+++ handTracking.py
@@ -2,7 +2,7 @@
 # https://stackoverflow.com/questions/66876906/create-a-rectangle-around-all-the-points-returned-from-mediapipe-hand-landmark-d
 # https://stackoverflow.com/questions/67455791/mediapipe-python-link-landmark-with-handedness
 # https://docs.python.org/3/library/csv.html
-# using termProject enviroment
+# using handTracking_hannah enviroment
 
 import cv2
 import csv
@@ -17,8 +17,28 @@ import numpy as np
 from scipy.spatial import cKDTree
 import torch
 
+#TODO
+#make default parameters and allow the user to pass in the gesture video name, name output files accordingly
+#load and process an entire directory of videos (wild animals dataset from last semester)
+
+def removeNewLines(value):
+    return value.replace('\n',' ')
+
 cuda = True if torch.cuda.is_available() else False
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+handInfoCsv = "/home/exx/hannah/GitProjects/microgesture/vid1/output.csv"
+if os.path.exists(handInfoCsv):
+  os.remove(handInfoCsv)
+
+frameOutputDirectory = "/home/exx/hannah/GitProjects/microgesture/vid1/frames/"
+if not os.path.exists(frameOutputDirectory):
+    os.makedirs(frameOutputDirectory)
+
+csvfile = open(handInfoCsv, 'w', newline='')
+fieldnames = ['Frame', "Landmarks"]
+writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+writer.writeheader()
 
 # initialize mediapipe
 mpHands = mp.solutions.hands
@@ -37,7 +57,7 @@ handCount = 0
 #cap = cv2.VideoCapture(0)
 
 # file
-cap = cv2.VideoCapture("D:\\MG Pilot Data\\DemoVideos_MG\\output-0.mkv")
+cap = cv2.VideoCapture("/data/MGDataset/Synthetic Data/Two/Two_0001.avi")
 
 frameCount = 0
 _, frame = cap.read()
@@ -46,9 +66,8 @@ h, w, c = frame.shape
 while cap.isOpened():
     success, frame = cap.read()
     if not success:
-        print("Ignoring empty camera frame.")
-        continue
-    frameCount+=1
+        print("Empty camera frame, assuming end of file.")
+        break
     frame = cv2.flip(frame, 1)
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -58,7 +77,6 @@ while cap.isOpened():
     handedness_result = result_hands.multi_handedness
     hands_world_result = result_hands.multi_hand_world_landmarks
   
-
     if handedness_result:
         print('Handedness:', handedness_result)
     if hands_result:
@@ -80,9 +98,14 @@ while cap.isOpened():
                     y_min = y
 
             mpDraw.draw_landmarks(frame, hand, mpHands.HAND_CONNECTIONS)
+            writer.writerow({'Frame': str(frameCount), 'Landmarks': removeNewLines(str(hand.landmark))})
 
     cv2.putText(frame, "Frame: " + str(frameCount), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
-    cv2.imshow("Frame", frame)
+    #cv2.imshow("Frame", frame)
+    cv2.imwrite(frameOutputDirectory + "Frame" + str(frameCount) + ".png", frame)
+
+    frameCount+=1
+
     if cv2.waitKey(1) == ord('q'):
         break
 
